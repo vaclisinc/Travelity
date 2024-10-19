67332d1368a28f7a1903327b5dcb2c8f4e8f8ee7
SimonLiu <simonliu423@gmail.com>
2024-10-19 12:35:54 +0800

    Refactor: remove '_' for variable

--- lib/features/introduction/setup/bio_setup.dart
+++ lib/features/introduction/setup/bio_setup.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:travelity/constant/tags.dart';
 import 'package:travelity/core/model/tags_m.dart';
 import 'package:travelity/features/introduction/bloc/user_setup_bloc.dart';
 
@@ -15,13 +14,13 @@ class _BioSetupState extends State<BioSetup> {
   List<InterestTag> selectedTags = [];
   @override
   Widget build(BuildContext context) {
-    TextEditingController _controller = TextEditingController();
+    TextEditingController controller = TextEditingController();
 
     return BlocBuilder<UserSetupBloc, UserSetupState>(
       builder: (context, state) {
-        _controller.text = state.user.bio ?? '';
+        controller.text = state.user.bio ?? '';
         return TextField(
-          controller: _controller,
+          controller: controller,
           decoration: const InputDecoration(
             hintText: '請輸入您的簡單介紹',
           ),
