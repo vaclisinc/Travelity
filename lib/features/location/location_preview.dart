0c342526706462e7d8b8fab5add0da04ebf04932
SimonLiu <simonliu423@gmail.com>
2024-10-20 04:49:18 +0800

    Feat: navigate to location details from preview

--- lib/features/location/location_preview.dart
+++ lib/features/location/location_preview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travelity/core/model/location_m.dart';
+import 'package:travelity/features/location/location_detail.dart';
 
 class LocationPreview extends StatelessWidget {
   const LocationPreview({super.key, required this.location});
@@ -8,36 +9,47 @@ class LocationPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Image.network(
-              width: 150,
-              height: 150,
-              location.imageUrl,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(width: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  location.name,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LocationDetailPage(location: location),
+            fullscreenDialog: true,
+          ),
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Image.network(
+                width: 150,
+                height: 150,
+                location.imageUrl,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    location.name,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  location.description,
-                  style: const TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(height: 10),
+                  Text(
+                    location.description,
+                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
